9cb6527dc2b15e0b2a2d8fc06b0171c14acc1b51
root <root@HPE-5CG2184QNH.localdomain>
2026-01-20 22:04:05 +0530

    Adding storage account for sql audit

--- Infra/terraform/sql_auditing.tf
+++ Infra/terraform/sql_auditing.tf
@@ -1,7 +1,8 @@
-resource "azurerm_mssql_server_auditing_policy" "audit" {
-  server_id                  = azurerm_mssql_server.sql.id
-  storage_endpoint           = azurerm_storage_account.audit.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.audit.primary_access_key
-  retention_in_days          = 90
+resource "azurerm_mssql_server_extended_auditing_policy" "audit" {
+  server_id                               = azurerm_mssql_server.sql.id
+  storage_endpoint                       = azurerm_storage_account.audit.primary_blob_endpoint
+  storage_account_access_key             = azurerm_storage_account.audit.primary_access_key
+  retention_in_days                      = 90
+  storage_account_access_key_is_secondary = false
 }
 
